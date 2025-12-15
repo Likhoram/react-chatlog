1a506be2d99b68bde7422b3b2fa4b12685d84e24
Wenxin Li <likhoram24@gmail.com>
2025-12-14 21:20:36 -0600

    wave 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css';
+import ChatEntry from './components/ChatEntry';
+import messages from './data/messages.json';
 
 const App = () => {
   return (
     <div id="App">
       <header>
-        <h1>Application title</h1>
+        <h1>Chat between Vladimir and Estragon</h1>
       </header>
       <main>
-        {/* Wave 01: Render one ChatEntry component
-        Wave 02: Render ChatLog component */}
+        <ChatEntry
+          sender={messages[0].sender}
+          body={messages[0].body}
+          timeStamp={messages[0].timeStamp}
+        />
       </main>
     </div>
   );
